c09385332a7cbf78dfb7626e8201e10469801b38
marah19-meet <marah19@meet.mit.edu>
2017-07-26 17:54:16 +0200

    Stopping here

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,5 @@
-i=1
-for i in range(100)
-count=i+1
+
+for i in range(100):
+    count=i+1
+    print(count)
 
--- turtle_party.py
+++ turtle_party.py
@@ -1,12 +1,14 @@
 strawberries=50
-is_weekend=true
-if is_weekend:
-    if  > 39 < 1:
+is_weekend=True
+if is_weekend:#weekend
+   
+    if strawberries > 39:
         print('Fun!')
     else:
         print('Not Fun')
-else:
-    if s >39:
+else:#weekday
+   
+    if strawberries >39 and strawberries<61:
         print('Fun')
         
     else:
